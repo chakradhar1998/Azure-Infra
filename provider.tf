8b5d9fd8e8d1953b98fbf5e625b45e8a1f560b08
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2024-05-25 22:18:23 +0530

    removed rg in features

--- provider.tf
+++ provider.tf
@@ -1,9 +1,5 @@
 provider "azurerm" {
-  features = {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
+  features = {}
 }
 
 # Set the Azure Provider source and version being used
