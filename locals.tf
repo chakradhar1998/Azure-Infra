b388df07b2f41a0be874a9491366e1e50cdf9122
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2024-06-12 17:50:06 +0530

    subnet resource

--- locals.tf
+++ locals.tf
@@ -5,4 +5,14 @@ locals {
     name          = "app-network"
     address_space = "10.0.0.0/16"
   }
+  subnet = [
+    {
+      name           = "subnetA"
+      address_prefix = "10.0.0.0/24"
+    },
+    {
+      name           = "subnetB"
+      address_prefix = "10.0.1.0/24"
+    }
+  ]
 }
--- main.tf
+++ main.tf
@@ -9,14 +9,24 @@ resource "azurerm_virtual_network" "appnetwork" {
   resource_group_name = local.resource_group_name
   address_space       = [local.virtual_network.address_space]
 
-  subnet {
-    name           = "subnetA"
-    address_prefix = "10.0.0.0/24"
-  }
 
   subnet {
     name           = "subnetB"
     address_prefix = "10.0.1.0/24"
   }
   depends_on = [azurerm_resource_group.appgrp]
+}
+
+resource "azurerm_subnet" "subnetA" {
+  name                 = local.subnet[0].name
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = local.virtual_network.name
+  address_prefixes     = [local.subnet[0].address_prefix]
+}
+
+resource "azurerm_subnet" "subnetB" {
+  name                 = local.subnet[1].name
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = local.virtual_network.name
+  address_prefixes     = [local.subnet[1].address_prefix]
 }
