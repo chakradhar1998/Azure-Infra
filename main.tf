9ed7033ee87b52cb753e5648c70d6341362abf99
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2024-06-12 16:14:51 +0530

    change

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ resource "azurerm_resource_group" "appgrp" {
   name     = "app-grp"
   location = "westus2"
 }
+
+resource "azurerm_storage_account" "appstore2278370" {
+  name                     = "appstore2278370"
+  resource_group_name      = azurerm_resource_group.appgrp.name
+  location                 = azurerm_resource_group.appgrp.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  access_tier = "StorageV2"
+}
