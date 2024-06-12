20e965c759fdf2f1b7bfe28ad805b88fe97c82c0
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2024-06-12 21:08:32 +0530

    remove vm

--- main.tf
+++ main.tf
@@ -75,26 +75,27 @@ resource "azurerm_subnet_network_security_group_association" "appnsglink" {
   network_security_group_id = azurerm_network_security_group.appnsg.id
 }
 
-resource "azurerm_windows_virtual_machine" "appvm" {
-  name                = "appvm"
-  resource_group_name = local.resource_group_name
-  location            = local.location
-  size                = "Standard_D2S_v3"
-  admin_username      = "adminuser"
-  admin_password      = "P@$$w0rd1234!"
-  network_interface_ids = [
-    azurerm_network_interface.appinterface.id,
-  ]
+# resource "azurerm_windows_virtual_machine" "appvm" {
+#   name                = "appvm"
+#   resource_group_name = local.resource_group_name
+#   location            = local.location
+#   size                = "Standard_D2S_v3"
+#   admin_username      = "adminuser"
+#   admin_password      = "P@$$w0rd1234!"
+#   network_interface_ids = [
+#     azurerm_network_interface.appinterface.id,
+#   ]
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
 
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
-    version   = "latest"
-  }
-}
+#   source_image_reference {
+#     publisher = "MicrosoftWindowsServer"
+#     offer     = "WindowsServer"
+#     sku       = "2019-Datacenter"
+#     version   = "latest"
+#   }
+#   depends_on = [ azurerm_network_interface.appinterface ]
+# }
