ca9d58ad67295bfc332c49c61de694f6cf84edfe
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2024-06-12 20:20:04 +0530

    public ip

--- main.tf
+++ main.tf
@@ -34,15 +34,19 @@ resource "azurerm_network_interface" "appinterface" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnetA.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.appip.id
   }
   depends_on = [azurerm_subnet.subnetA]
 }
 
-# resource "azurerm_public_ip" "example" {
-#   name                = "acceptanceTestPublicIp1"
-#   resource_group_name = local.resource_group_name
-#   location            = local.location
-#   allocation_method   = "Dynamic"
+resource "azurerm_public_ip" "appip" {
+  name                = "app-ip"
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  allocation_method   = "Dynamic"
 
-
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+  depends_on = [ local.resource_group_name ]
+}
