982fbaae71eb6f4eb3bf6510ef3f2841daf64131
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2024-06-12 22:03:26 +0530

    linux vm

--- main.tf
+++ main.tf
@@ -25,90 +25,92 @@ resource "azurerm_subnet" "subnetB" {
   address_prefixes     = [local.subnet[1].address_prefix]
 }
 
-# resource "azurerm_network_interface" "appinterface" {
-#   name                = "appinterface"
-#   location            = local.location
-#   resource_group_name = local.resource_group_name
+resource "azurerm_network_interface" "appinterface" {
+  name                = "appinterface"
+  location            = local.location
+  resource_group_name = local.resource_group_name
 
-#   ip_configuration {
-#     name                          = "internal"
-#     subnet_id                     = azurerm_subnet.subnetA.id
-#     private_ip_address_allocation = "Dynamic"
-#     public_ip_address_id          = azurerm_public_ip.appip.id
-#   }
-#   depends_on = [azurerm_subnet.subnetA]
-# }
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnetA.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.appip.id
+  }
+  depends_on = [azurerm_subnet.subnetA]
+}
 
-# resource "azurerm_network_interface" "appinterface2" {
-#   name                = "appinterface2"
-#   location            = local.location
-#   resource_group_name = local.resource_group_name
 
-#   ip_configuration {
-#     name                          = "internal"
-#     subnet_id                     = azurerm_subnet.subnetA.id
-#     private_ip_address_allocation = "Dynamic"
-#   }
-#   depends_on = [azurerm_subnet.subnetA]
-# }
+resource "azurerm_public_ip" "appip" {
+  name                = "app-ip"
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  allocation_method   = "Dynamic"
 
-# resource "azurerm_public_ip" "appip" {
-#   name                = "app-ip"
-#   resource_group_name = local.resource_group_name
-#   location            = local.location
-#   allocation_method   = "Dynamic"
+  lifecycle {
+    create_before_destroy = true
+  }
+  depends_on = [local.resource_group_name]
+}
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-#   depends_on = [local.resource_group_name]
-# }
+resource "azurerm_network_security_group" "appnsg" {
+  name                = "app-nsg"
+  location            = local.location
+  resource_group_name = local.resource_group_name
 
-# resource "azurerm_network_security_group" "appnsg" {
-#   name                = "app-nsg"
-#   location            = local.location
-#   resource_group_name = local.resource_group_name
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  depends_on = [azurerm_resource_group.appgrp]
+}
 
-#   security_rule {
-#     name                       = "AllowRDP"
-#     priority                   = 300
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "3389"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
-#   depends_on = [azurerm_resource_group.appgrp]
-# }
+resource "azurerm_subnet_network_security_group_association" "appnsglink" {
+  subnet_id                 = azurerm_subnet.subnetA.id
+  network_security_group_id = azurerm_network_security_group.appnsg.id
+}
 
-# resource "azurerm_subnet_network_security_group_association" "appnsglink" {
-#   subnet_id                 = azurerm_subnet.subnetA.id
-#   network_security_group_id = azurerm_network_security_group.appnsg.id
-# }
+resource "tls_private_key" "linuxkey" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
-# resource "azurerm_windows_virtual_machine" "appvm" {
-#   name                = "appvm"
-#   resource_group_name = local.resource_group_name
-#   location            = local.location
-#   size                = "Standard_D2S_v3"
-#   admin_username      = "adminuser"
-#   admin_password      = "P@$$w0rd1234!"
-#   network_interface_ids = [
-#     azurerm_network_interface.appinterface.id, azurerm_network_interface.appinterface2.id
-#   ]
+resource "local_file" "linuxpemkey" {
+  content    = tls_private_key.linuxkey.private_key_pem
+  filename   = "linuxkey.pem"
+  depends_on = [tls_private_key.linuxkey]
+}
 
-#   os_disk {
-#     caching              = "ReadWrite"
-#     storage_account_type = "Standard_LRS"
-#   }
+resource "azurerm_linux_virtual_machine" "linuxvm" {
+  name                = "linuxvm"
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
 
-#   source_image_reference {
-#     publisher = "MicrosoftWindowsServer"
-#     offer     = "WindowsServer"
-#     sku       = "2019-Datacenter"
-#     version   = "latest"
-#   }
-#   depends_on = [azurerm_network_interface.appinterface]
-# }
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.linuxkey.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
