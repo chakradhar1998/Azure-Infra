ce87d3af6818ac6d20366c2f87d9276aa9ff450c
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2024-06-12 16:33:14 +0530

    blob

--- main.tf
+++ main.tf
@@ -11,3 +11,17 @@ resource "azurerm_storage_account" "appstore2278370" {
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
 }
+
+resource "azurerm_storage_container" "data" {
+  name                  = "data"
+  storage_account_name  = azurerm_storage_account.appstore2278370.name
+  container_access_type = "blob"
+}
+
+resource "azurerm_storage_blob" "maintf" {
+  name                   = "main.tf"
+  storage_account_name   = azurerm_storage_account.appstore2278370.name
+  storage_container_name = azurerm_storage_container.data.name
+  type                   = "Block"
+  source                 = "main.tf"
+}
