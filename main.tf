429b55acaeebd1f2fcc0f003888a5a5c2c96b9dc
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2024-06-12 17:50:35 +0530

    change

--- main.tf
+++ main.tf
@@ -8,12 +8,6 @@ resource "azurerm_virtual_network" "appnetwork" {
   location            = local.location
   resource_group_name = local.resource_group_name
   address_space       = [local.virtual_network.address_space]
-
-
-  subnet {
-    name           = "subnetB"
-    address_prefix = "10.0.1.0/24"
-  }
   depends_on = [azurerm_resource_group.appgrp]
 }
 
