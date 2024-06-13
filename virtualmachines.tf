65c80ad9d6a938bc189fc8c394d410af99d8b16f
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2024-06-13 23:37:54 +0530

    vm deploy

--- virtualmachines.tf
+++ virtualmachines.tf
@@ -25,3 +25,29 @@ resource "azurerm_public_ip" "appip" {
   }
   depends_on = [local.resource_group_name]
 }
+
+resource "azurerm_windows_virtual_machine" "appvm" {
+  count               = var.number_of_machines
+  name                = "appvm${count.index}"
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  size                = "Standard_D2S_v3"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.appinterface[count.index].id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+  depends_on = [azurerm_network_interface.appinterface]
+}
