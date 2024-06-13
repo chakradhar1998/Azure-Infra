60247fca10b9c68d34ec40a3f1267269a861663f
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2024-06-13 21:41:34 +0530

    count

--- main.tf
+++ main.tf
@@ -4,3 +4,14 @@ resource "azurerm_resource_group" "appgrp" {
 }
 
 
+resource "azurerm_storage_account" "appstore2278370" {
+  count = 3
+  name                     = "${count.index}appstore2278370"
+  resource_group_name      = local.resource_group_name
+  location                 = local.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind = "StorageV2"
+  depends_on = [ azurerm_resource_group.appgrp ]
+
+}
