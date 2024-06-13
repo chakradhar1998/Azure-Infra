fce4296315a0f62190ddb84c2e870601c9089973
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2024-06-13 23:10:31 +0530

    nsg

--- networking.tf
+++ networking.tf
@@ -14,3 +14,29 @@ resource "azurerm_subnet" "subnets" {
   address_prefixes     = ["10.0.${count.index}.0/24"]
   depends_on           = [azurerm_virtual_network.appnetwork]
 }
+
+resource "azurerm_network_security_group" "appnsg" {
+  name                = "app-nsg"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  security_rule {
+    name                       = "AllowRDP"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  depends_on = [azurerm_resource_group.appgrp]
+}
+
+resource "azurerm_subnet_network_security_group_association" "appnsglink" {
+  count                     = var.number_of_subnets
+  subnet_id                 = azurerm_subnet.subnets[count.index].id
+  network_security_group_id = azurerm_network_security_group.appnsg.id
+  depends_on                = [azurerm_virtual_network.appnetwork, azurerm_network_security_group.appnsg]
+}
--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "number_of_subnets" {
   description = "This defines the number of subnets i want"
   default     = 2
   validation {
-    condition = var.number_of_subnets < 5
+    condition     = var.number_of_subnets < 5
     error_message = "The nmber of subnet must be less than 5."
   }
 
