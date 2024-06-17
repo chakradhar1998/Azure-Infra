735e3627107c8f95391ac373982d95bbb486307c
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2024-06-18 00:12:37 +0530

    delete resource in default sub

--- backend.tf
+++ backend.tf
@@ -4,6 +4,5 @@ terraform {
     storage_account_name = "tfstate2408"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
-    subscription_id = "55b24198-da47-4df9-a82d-aa987405a685"
   }
 }
