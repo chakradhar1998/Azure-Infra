a8d51d43a591fceac2e232d829de9ba4ecf875ec
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2024-06-19 22:36:51 +0530

    saved the vnet change

--- modules/vnet/vnet.tf
+++ modules/vnet/vnet.tf
@@ -4,11 +4,3 @@ resource "azurerm_virtual_network" "appnetwork" {
   resource_group_name = var.resource_group_name
   address_space       = var.address_space
 }
-
-resource "azurerm_subnet" "subnets" {
-  name                 = var.subnetname
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnetname
-  address_prefixes     = var.address_prefix
-  depends_on           = [azurerm_virtual_network.appnetwork]
-}
