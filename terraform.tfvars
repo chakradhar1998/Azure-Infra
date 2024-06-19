f97e307af815883e010c92de99306d4c735d9223
Chakradhar Gorthi <chakradhar.gorthi@gmail.com>
2024-06-19 18:48:03 +0530

    NIC cards deploy in sponsered sub

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,1 @@
-number_of_machines = 3
 subscription_id    = "55b24198-da47-4df9-a82d-aa987405a685"
--- virtualmachines.tf
+++ virtualmachines.tf
@@ -1,17 +1,17 @@
-# resource "azurerm_network_interface" "appinterface" {
-#   count               = var.number_of_machines
-#   name                = "appinterface${count.index}"
-#   location            = local.location
-#   resource_group_name = local.resource_group_name
+resource "azurerm_network_interface" "appinterface" {
+  count               = var.number_of_machines
+  name                = "appinterface${count.index}"
+  location            = local.location
+  resource_group_name = local.resource_group_name
 
-#   ip_configuration {
-#     name                          = "internal"
-#     subnet_id                     = azurerm_subnet.subnets[count.index].id
-#     private_ip_address_allocation = "Dynamic"
-#     public_ip_address_id          = azurerm_public_ip.appip[count.index].id
-#   }
-#   depends_on = [azurerm_subnet.subnets, azurerm_public_ip.appip]
-# }
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnets[count.index].id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.appip[count.index].id
+  }
+  depends_on = [azurerm_subnet.subnets, azurerm_public_ip.appip]
+}
 
 # resource "azurerm_public_ip" "appip" {
 #   count               = var.number_of_machines
